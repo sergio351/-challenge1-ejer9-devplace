000c568b138c6ca32417da2236282435b7277a0c
sergio351 <velazquezsergio714@gmail.com>
2022-05-08 20:46:28 -0300

    primercommit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace _9
+{
+    class Program
+    {
+        static void Main(string[] args )
+        {
+            float basemayor,altura,basemenor,area;
+            Console.WriteLine("Ingresar base mayor");
+            basemayor=float.Parse(System.Console.ReadLine());
+            Console.WriteLine("Ingresar base menor");
+            basemenor=float.Parse(System.Console.ReadLine());
+            Console.WriteLine("Ingresar altura");
+            altura=float.Parse(System.Console.ReadLine());
+            area=((basemenor+basemayor)/2+altura);
+            Console.WriteLine("El area es: "+area);
+
+        }
+    }
+}
